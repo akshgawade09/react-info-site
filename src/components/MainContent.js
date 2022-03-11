ed1b23a58e78270610b716e520deebc678826d8d
akshgawade09 <akshgawade09@gmail.com>
2022-03-12 02:13:05 +0530

    First Build

---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function MainContent() {
+    return (
+        <>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>React is a popular library.</li>
+                <li>If a learn React, I can get the job of Full Stack Developer.</li>
+            </ol>
+        </>
+    )
+}
