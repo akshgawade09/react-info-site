1bcabcca812ce882a031f494413d8c326a0b15f1
akshgawade09 <akshgawade09@gmail.com>
2022-03-12 11:05:02 +0530

    Alt img property

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Logo from '../images/logo.svg';
 export default function Navbar() {
     return (
         <nav>
-            <img src={Logo}></img>
+            <img src={Logo} alt='React Logo'></img>
             <h3 className='nav--logo_text'>ReactFacts</h3>
             <h4 className='nav--title'>React Course - Project 1</h4>
         </nav>
