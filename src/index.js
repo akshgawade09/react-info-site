e400c4577cccfee00f867d0a3251d68a450ac0e0
akshgawade09 <akshgawade09@gmail.com>
2022-03-12 10:44:36 +0530

    Static React Website Final Build

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MainContent from './components/MainContent';
+import App from './App';
 
-function Page() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    )
-}
-
-ReactDOM.render(<Page />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
